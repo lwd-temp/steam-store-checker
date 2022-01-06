fa7824c0cdeda8c46c8330fb89edc0981656fc93
lwd-temp <26019675+lwd-temp@users.noreply.github.com>
2022-01-06 23:22:02 +0800

    Verbose

--- v2ex/curl测试 - 对照组.bat
+++ v2ex/curl测试 - 对照组.bat
@@ -4,7 +4,7 @@ echo 请按下任意键测试一次
 pause
 :curl
 title curl测试 - 对照组 - 请求中
-curl -I -k --resolve store.steampowered.com:443:34.227.211.26 https://store.steampowered.com
+curl -I -k -v --resolve store.steampowered.com:443:34.227.211.26 https://store.steampowered.com
 title curl测试 - 对照组 - 请求结束
 echo 执行结束
 echo 请按下任意键再测试一次
--- v2ex/curl测试.bat
+++ v2ex/curl测试.bat
@@ -4,7 +4,7 @@ echo 请按下任意键测试一次
 pause
 :curl
 title curl测试 - 请求中
-curl -I -k --resolve store.steampowered.com:443:23.42.182.65 https://store.steampowered.com
+curl -I -k -v --resolve store.steampowered.com:443:23.42.182.65 https://store.steampowered.com
 title curl测试 - 请求结束
 echo 执行结束
 echo 请按下任意键再测试一次
