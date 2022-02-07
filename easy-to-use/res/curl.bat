55c7ae7e39ae5d1acb865e51c27d1c4f086a051f
lwd-temp <26019675+lwd-temp@users.noreply.github.com>
2022-02-07 22:22:56 +0800

    better batch

--- easy-to-use/res/curl.bat
+++ easy-to-use/res/curl.bat
@@ -10,8 +10,8 @@ set basetitle=curl - %domain%解析为%ip%
 
 title %basetitle%
 
-echo curl网站访问测试
-echo 在本测试中，我们会尝试访问%url%，其中%domain%被解析为%ip%。
+echo curl %ip%网站访问测试
+echo 在本测试中，我们会以访问%domain%的名义尝试访问服务器%ip%。URL为%url%，其中%domain%被解析为%ip%。
 echo 请按下任意键测试一次
 pause
 
--- easy-to-use/res/tcping.bat
+++ easy-to-use/res/tcping.bat
@@ -6,7 +6,7 @@ set basetitle=tcping - %ip%:%port%
 
 title %basetitle%
 
-echo tcping服务器端口TCP协议可达性测试
+echo tcping %ip%服务器%port%端口TCP协议可达性测试
 echo 在本测试中，我们会不断测试%ip%:%port%是否可达。若不可达，则会提示“No Response”。
 echo 请仔细观察下方输出信息。
 
